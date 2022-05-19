31ef962d426a87f5f9d380904c14abac3b91c995
Jhordany Polanco <74990160+JhordP@users.noreply.github.com>
2022-05-19 07:51:35 -0400

    Agregado: manejo enums (Clase Dias)

---
+++ src/enums/Dias.java
@@ -0,0 +1,14 @@
+//package enums;
+
+public enum Dias { //Se utiliza la keyword enum en vez de class
+    /*Todos los elementos agregados a un enum son por defecto public, static y final
+    por lo que son constantes automaticamente. El default datatype es String, asi que
+    imprime el nombre de la variable pero en formato string*/
+    LUNES, 
+    MARTES,
+    MIERCOLES,
+    JUEVES,
+    VIERNES,
+    SABADO,
+    DOMINGO
+}
