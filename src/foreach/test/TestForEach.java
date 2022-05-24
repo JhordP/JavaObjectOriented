7bef427060cad2fd18ec8912950ecdff56b4378c
JhordP <j.horda.ny@hotmail.com>
2022-05-24 18:14:39 -0400

    ForEach con objetos

--- src/foreach/test/TestForEach.java
+++ src/foreach/test/TestForEach.java
@@ -1,4 +1,5 @@
 package foreach.test;
+import foreach.domain.Persona;
 
 public class TestForEach {
     public static void main(String[] args) {
@@ -7,6 +8,13 @@ public class TestForEach {
         //Una variable edad del tipo del arreglo, dos puntos y el arreglo.
         for (int edad: edades) {
             System.out.println(edad);
+        }
+
+        Persona personas[] = 
+        {new Persona("Juan"), new Persona("Karla"), new Persona("Agustin")};
+
+        for(Persona persona : personas) {
+            System.out.println(persona); //cada "persona" es la instancia asi que se imprime el toString
         }
     }
 }
