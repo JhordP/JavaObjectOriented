f9f7d60c5eb5024d0af41c938f889a2aa0a180e7
Jhordany Polanco <74990160+JhordP@users.noreply.github.com>
2022-06-02 11:06:28 -0400

    Creado: Gerente.java [instanceof]

---
+++ src/instanceof/domain/Gerente.java
@@ -0,0 +1,16 @@
+package sobreescritura.metodos.domain;
+public class Gerente extends Empleado {
+    
+    private String departamento;
+    
+    public Gerente(String nombre, double sueldo, String departamento) {
+        super(nombre, sueldo);
+        this.departamento = departamento;
+    }
+
+    @Override //Se sobreescribe el metodo desde la clase padre. Lo correcto es usar la notación Override.
+    public String obtenerDetalles() {
+        //Se puede utilizar supero para utilizar lo que ya retorna la clase padre y entonces agregar la sobreescritura.
+        return super.obtenerDetalles() + ", " + this.departamento;
+    }
+}
