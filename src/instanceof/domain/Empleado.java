3f154df4e4c7f3e00864fe74d3c5a3f5102348cc
Jhordany Polanco <74990160+JhordP@users.noreply.github.com>
2022-06-02 11:05:38 -0400

    Creado: Empleado.java [instanceof]

---
+++ src/instanceof/domain/Empleado.java
@@ -0,0 +1,23 @@
+package sobreescritura.metodos.domain;
+
+public class Empleado {
+    protected String nombre;
+    protected double sueldo;
+
+    public Empleado(String nombre, double sueldo) {
+         this.nombre = nombre;
+         this.sueldo = sueldo;
+    }
+
+    public String obtenerDetalles() {
+        return this.nombre+", "+String.valueOf(this.sueldo);
+    }
+
+    public String getNombre() {
+        return this.nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+}
