d8fdc268c819392aff55eaa36f3f7998960279b1
JhordP <j.horda.ny@hotmail.com>
2022-06-11 10:51:08 -0400

    Se agrego HashCode y Equals a Empleado

--- src/object/test/TestObject.java
+++ src/object/test/TestObject.java
@@ -1,7 +1,12 @@
 package object.test;
 
+import casting.domain.Empleado;
+
 public class TestObject {
     public static void main(String[] args) {
-        
+        Empleado emp1 = new Empleado("Juan", 5000.00);
+        Empleado emp2 = new Empleado("Juan", 5000.00);
+
+        System.out.println(emp1.equals(emp2));
     }
 }
