d1c11c043300a50bd8cbd17ebd11f265397c2180
Jhordany Polanco <74990160+JhordP@users.noreply.github.com>
2022-06-13 12:19:01 -0400

    Agregada comparación .equals() en TestCasting

--- src/object/test/TestObject.java
+++ src/object/test/TestObject.java
@@ -1,12 +1,16 @@
 package object.test;
 
-import casting.domain.Empleado;
-
-public class TestObject {
+public class Main {
     public static void main(String[] args) {
         Empleado emp1 = new Empleado("Juan", 5000.00);
         Empleado emp2 = new Empleado("Juan", 5000.00);
-
-        System.out.println(emp1.equals(emp2));
+        
+        //Se evalua la referencia de memoria como tal del objeto
+        if(emp1 == emp2) System.out.println("Los objetos tienen la misma referencia de memoria.");
+        else System.out.println("Los objetos tienen distinta referencia de memoria.");
+        
+        //Se evalua el contenido del objeto utilizando el metodo sobreescrito .equals()
+        if (emp1.equals(emp2)) System.out.println("Los objetos son iguales en contenido.");
+        else System.out.println("Los objetos son distintos en contenido.");
     }
 }
