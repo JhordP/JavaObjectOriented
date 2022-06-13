22fb2452ca59ccd38dbb777fd6609fe364e20577
Jhordany Polanco <74990160+JhordP@users.noreply.github.com>
2022-06-13 12:38:35 -0400

    Agregada comparación .hashCode() en TestCasting

--- src/object/test/TestObject.java
+++ src/object/test/TestObject.java
@@ -12,5 +12,10 @@ public class Main {
         //Se evalua el contenido del objeto utilizando el metodo sobreescrito .equals()
         if (emp1.equals(emp2)) System.out.println("Los objetos son iguales en contenido.");
         else System.out.println("Los objetos son distintos en contenido.");
+        
+        //Evaluacion del HashCode (contenido de cada atributo). Tanto equals como HashCode discriminan entre Mayus y Minus.
+        //NOTA: Recordar-> HashCode trae un numero entero unico con poca probabilidad de repeticion para cada valor de objeto dando un valor final.
+        if(emp1.hashCode() == emp2.hashCode()) System.out.println("El valor HashCode es igual.");
+        else System.out.println("El valor HashCode es distinto.");
     }
 }
