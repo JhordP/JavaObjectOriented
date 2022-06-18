34a068e2cd4260668f84987dcc698ca2cfe03f91
JhordP <j.horda.ny@hotmail.com>
2022-06-17 22:39:15 -0400

    Excepcion division por 0

--- src/excepcions/manejoexcepciones1/test/TestExcepciones.java
+++ src/excepcions/manejoexcepciones1/test/TestExcepciones.java
@@ -2,6 +2,17 @@ package excepcions.manejoexcepciones1.test;
 
 public class TestExcepciones {
     public static void main(String[] args) {
+
+        int resultado = 0;
+        try {
+            resultado = 10/0;
+            System.out.println("Resultado: "+resultado);
+        } catch (Exception e) {
+            System.out.println("Ocurrio un error:\n ");
+            //Imprime la pila de excepciones
+            e.printStackTrace(System.out);
+        }
+        System.out.println(resultado);
         
     }
 }
