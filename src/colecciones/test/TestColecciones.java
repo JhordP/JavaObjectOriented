85bd2309dca987c6d4e135153753d228573bc8ff
Jhordany Polanco <74990160+JhordP@users.noreply.github.com>
2022-06-21 13:08:47 -0400

    Update: Se agrego Maps y Notas finales.

--- src/colecciones/test/TestColecciones.java
+++ src/colecciones/test/TestColecciones.java
@@ -33,6 +33,35 @@ public class TestColecciones{
             En las Listas los elementos pueden duplicarse
             En los Set los elementos duplicados se sobreescriben
         */
+        
+        Map miMapa = new HashMap();
+        System.out.println("\nMap:");
+        
+        //Se agrega una "key" y un registro , ambos de cualquier tipo de dato Object.
+        miMapa.put("P1", "Juan");
+        miMapa.put(02, "Karla");
+        miMapa.put('3', "Roberto");
+        //miMapa.put(02, "Jose"); Si se le coloca la misma key a otro valor, se sobreescribe.
+        
+        //Se trae la key como parametro sin importar el tipo de dato.
+        String elemento = (String)miMapa.get(02); //Se realiza un DownCasting para poder almacenarlo en la variable.
+        System.out.println(elemento);
+        //System.out.println((String)miMapa.get('3')); //TryHard 1 linea
+        
+        System.out.println("\nTrayendo todos los elementos\n");
+        //Para traer todas las llaves:
+        Set mapa = miMapa.keySet(); //Devuelve un set por lo que el orden varia. No devuelve los valores sino las Key.
+        mapa.forEach(elem -> { System.out.println(elem); });
+        
+        //Para traer todas los valores:
+        Collection lista = miMapa.values();
+        lista.forEach(elem -> { System.out.println(elem); });
+        
+        /*Diferencias-->
+            List: Guardan el orden o index en el que fueron agregados
+            Set: No guardan el orden del index que fueron agregados
+            Map: Guardan una llave de referencia en cada valor.
+        */
     }
     
     //Aplicando polimorfismo
