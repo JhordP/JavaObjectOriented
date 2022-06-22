ce5e3ad19e47810277a8d36c975e1c17308208cd
Jhordany Polanco <74990160+JhordP@users.noreply.github.com>
2022-06-22 12:04:49 -0400

    Creada TestGenerics [generics]

---
+++ src/generics/test/TestGenerics.java
@@ -0,0 +1,25 @@
+package generics.test;
+
+import java.util.Scanner;
+
+public class TestGenerics {
+    public static void main () {
+        var input = new Scanner(System.in);
+        System.out.println("Digite un valor.");
+        var valor = input.nextLine();
+        
+        //Se indica el tipo de objeto que utilizará la clase generica, y se crea una instancia en este caso llenando el constructor con su tipo.
+        try {
+        ClaseGenerica<Integer> objetoInt = new ClaseGenerica(Integer.parseInt(valor));
+        objetoInt.obtenerTipo();
+        }catch(Exception e){
+            e.getMessage();
+        }
+        
+        ClaseGenerica<String> objetoString = new ClaseGenerica(valor);
+        objetoString.obtenerTipo();
+        
+        //Nota: El tipo del tipo que se envia al constructor en este caso debe ser un tipo compatible:
+        //Ej. Si el T es padre, el que se envia por parametro puede ser hijo. Si el T es una interfaz, el parametro puede ser una clase que la implemente.
+    }
+}
